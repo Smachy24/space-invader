362068cf8cec4424719ba8392a1c6272d3a3007e
Smachy <pro.diegocdossantos@gmail.com>
2023-03-13 16:40:34 +0100

    push down aliens

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,5 @@
 let mobs = setMobs();
+let direction = "right"
 
 function generateGrid(){
     const grid = document.querySelectorAll(".grille")[0];
@@ -43,15 +44,72 @@ function addMobs(){
 function clearMobs(){
     mobs.forEach(index =>{
         let box = document.querySelectorAll(".grille div")[index]
-        box.classList.remove("alien")
+        if(!box.getAttribute("data-right") && !box.getAttribute("data-left")){
+            box.classList.remove("alien")
+        }
+        
     })
 }
 
-function moveMobs(){
-    clearMobs();
+function setIndexNextLine(){
     for(let i=0; i<mobs.length; i++){
-        mobs[i]+=1
-    } 
+        console.log(i)
+        mobs[i]+=20
+    }
+}
+
+function moveIndex(){
+
+    let rightBoxes = document.querySelectorAll("div[data-right].alien")
+    let breakRight = false;
+    console.log(rightBoxes)
+    rightBoxes.forEach(element => {
+        console.log(element.className)
+        
+        console.log("1")
+        if(breakRight==false){
+            direction="left"
+            setIndexNextLine();
+        }
+        breakRight = true
+        
+        }
+    );
+    let leftBoxes = document.querySelectorAll("div[data-left] .alien")
+    let breakLeft = false;
+    leftBoxes.forEach(element => {
+            if(breakLeft==false){
+                direction="right"
+                setIndexNextLine();
+            }
+            box.classList.remove(element)
+            breakLeft = true
+            
+    });
+
+    if(direction=="right"){
+        if(breakRight==false){
+            for(let i=0; i<mobs.length; i++){
+                mobs[i]+=1
+            } 
+        }
+    }
+
+    if(direction=="left"){
+        if(breakLeft==false){
+            for(let i=0; i<mobs.length; i++){
+                mobs[i]-=1
+            } 
+        }
+    }
+
+    
+}
+
+function moveMobs(){
+    console.log(mobs)
+    clearMobs();
+    moveIndex()
     addMobs();
 }
 
